86d7fd82798861a355c7ead39cff530a2f121d50
Sebastian Stephenson <sebastian.stephenson1@gmail.com>
2021-10-30 22:57:06 -0400

    Created a digital clock
    I used an interval method to keep the clock going.
    I added a condition for AM or PM to show up, given the time of day.
    I added zeros to the hours, minutes and seconds; if they were less than 10.

--- index.js
+++ index.js
@@ -1,7 +1,44 @@
-function showTime(){
-  Date date = new Date();
-  
-  document.getElementById("time").innerText = date;
-}
+setInterval(showTime, 1000);
 
-showtime();
+function showTime(){
+  let date = new Date();
+
+  let hours = date.getHours();
+  let mins = date.getMinutes();
+  let secs = date.getSeconds();
+  let pmORam;
+
+  if(hours > 12){
+  	pmORam = "PM";
+
+  }else{
+  	pmORam = "AM";
+  }// Adds a pm or am given the time of day
+
+  
+  // These 3 if statements adds a 0 if the number is single
+  if(mins < 10){
+  	mins = `0${mins}`;
+  }
+
+  if(secs < 10){
+  	secs = `0${secs}`;
+  }
+
+  if(hours < 10){
+  	hours = `0${hours}`;
+  }
+  //---------------------------//
+
+  if(hours > 12){
+  	hours = hours - 12;
+  }// This gets rid of the military time
+
+  let time = `${hours}:${mins}:${secs} ${pmORam}`;
+
+  document.getElementById('time').innerHTML = time;
+
+}// End of the showTime function
+
+showTime();
+
